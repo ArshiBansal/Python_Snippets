9e752c0f04ffab6a0b8682770707f0bf4c89b0e9
ArshiBansal <arshi900389@gmail.com>
2025-06-03 11:24:39 +0530

    Sending message on WhatsApp using python

---
+++ 2.py
@@ -0,0 +1,4 @@
+import pywhatkit as pwk
+phone_number = "+919417090062"
+message = "Hello, this is an automated test message."
+pwk.sendwhatmsg(phone_number, message, 19, 15)  # Schedules the message for 19:15 IST
