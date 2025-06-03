dcdb56bc2b7931460ab04a7bb1241d206e57d175
Arshi <150040105+ArshiBansal@users.noreply.github.com>
2025-06-03 11:27:05 +0530

    resizing image using python

--- 1.py
+++ 1.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
 # Open the image
-img = Image.open('Cloud Computing-NPTEL_page-0001.jpg')
+img = Image.open('image.jpg')
 
 # Resize to new dimensions (e.g., 50% smaller)
 new_size = (int(img.width * 0.25), int(img.height * 0.25))
