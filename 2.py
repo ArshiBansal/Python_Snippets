b51b9c20bfe09f15704ea757170a89ec4f51e1a3
ArshiBansal <arshi900389@gmail.com>
2025-06-04 15:46:33 +0530

    commit

--- 2.py
+++
@@ -1,4 +0,0 @@
-import pywhatkit as pwk
-phone_number = "+919417090062"
-message = "Hello, this is an automated test message."
-pwk.sendwhatmsg(phone_number, message, 19, 15)  # Schedules the message for 19:15 IST
